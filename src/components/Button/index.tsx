fb946ce75217c63440cedf3af6c247911c9f6db1
quyenptt03 <quyenptt03@gmail.com>
2025-03-18 17:36:15 +0700

    Update Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,20 @@
 interface IButtonProps {
   onClick?: () => void;
+  className?: string;
   type?: "button" | "submit" | "reset";
   theme: "base" | "filled";
   children: any;
 }
 
 const Button = (props: IButtonProps) => {
-  const { theme, children, type, onClick } = props;
+  const { theme, children, type, onClick, className } = props;
   if (theme === "filled")
     return (
       <button
         type={type}
         onClick={onClick}
-        className="mt-4 px-8 py-4 font-semibold border border-solid border-transparent rounded-lg laptop:px-10 laptop:py-4 3xl:px-12 3xl:py-6 
-            text-white bg-black hover:text-black hover:bg-white hover:border-black transition-all duration-200 ease-linear w-full"
+        className={`mt-4 px-8 py-4 font-semibold border border-solid border-transparent rounded-lg laptop:px-10 laptop:py-4 3xl:px-12 3xl:py-6 
+            text-white bg-black hover:text-black hover:bg-white hover:border-black transition-all duration-200 ease-linear w-full ${className}`}
       >
         {children}
       </button>
@@ -23,8 +24,8 @@ const Button = (props: IButtonProps) => {
       <button
         type={type}
         onClick={onClick}
-        className="mt-4 px-8 py-4 font-semibold border border-solid border-transparent rounded-lg laptop:px-10 laptop:py-4 3xl:px-12 3xl:py-6 
-            text-black bg-white hover:text-white hover:bg-black hover:border-black transition-all duration-200 ease-linear w-full"
+        className={`mt-4 px-8 py-4 font-semibold border border-solid border-transparent rounded-lg laptop:px-10 laptop:py-4 3xl:px-12 3xl:py-6 
+            text-black bg-white hover:text-white hover:bg-black hover:border-black transition-all duration-200 ease-linear w-full ${className}`}
       >
         {children}
       </button>
